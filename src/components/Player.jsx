786f33cabb2954ee1a26ab9644ccd1bdf1f2365c
Sumant64 <sumant1995shah@gmail.com>
2025-02-27 17:57:56 +0530

    player container design

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Player = (props) => {
-    const {playerX, setPlayerX, playerO, setPlayerO, setDisplayBoard} = props;
+  const { playerX, setPlayerX, playerO, setPlayerO, setDisplayBoard } = props;
   return (
     <>
-        <div className='player-container'>
-            <div className='player-form-container'>
-                <div>
-                    <h1>Enter Player Names</h1>
-                    <label>Player X: <input className='player-input' value={playerX} onChange={(event) => setPlayerX(event.target.value)} /></label>
-                    <label>Player O: <input className='player-input' value={playerO} onChange={(event) => setPlayerO(event.target.value)} /></label>
-                    <div>
-                        <button onClick={() => setDisplayBoard(true)}>Display Board</button>
-                    </div>
-                </div>
+      <div className="player-container">
+        <div className="player-form-container">
+          <div>
+            <h1>Enter Player Names</h1>
+            <div className="player-input-container">
+              <label>
+                PLAYER X:{" "}
+                <input
+                  className="player-input"
+                  value={playerX}
+                  onChange={(event) => setPlayerX(event.target.value)}
+                />
+              </label>
+              <label>
+                PLAYER O:{" "}
+                <input
+                  className="player-input"
+                  value={playerO}
+                  onChange={(event) => setPlayerO(event.target.value)}
+                />
+              </label>
             </div>
+            <div className="btn-container">
+              <button
+                className="display-board"
+                onClick={() => setDisplayBoard(true)}
+              >
+                DISPLAY BOARD
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
