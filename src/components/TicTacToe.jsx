52869ef94ea68f862153d7f3c9068990050232ac
Sumant64 <sumant1995shah@gmail.com>
2025-02-28 16:12:37 +0530

    fixes

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -5,16 +5,14 @@ const TicTacToe = (props) => {
     const {playerX, playerO} = props;
     const {initialBoard, isPlayerX, handleBoxClick, getStatusMessage, resetGame} = useTicTacToe(playerX, playerO);
 
-    console.log(initialBoard)
-
   return (
     <>
-        {getStatusMessage() && <div className='status-message'>
+        <div className='status-message' style={{opacity: getStatusMessage() ? 1 : 0, display: getStatusMessage() ? 'flex' : 'none' }}>
             <div className='status-heading'>
                 <h3>{getStatusMessage() === "X" ? `${playerX} Wins!` : `${playerO} Wins!`}</h3>
                 <button onClick={() => resetGame()} className='reset'>RESET</button>
             </div>
-        </div>}
+        </div>
         <main>
             <div className='box-container'>
             <div className='title-container'>
